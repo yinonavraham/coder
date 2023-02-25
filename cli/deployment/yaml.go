f918f860a87d8815fc5865052da3e8b2584547a6
Ammar Bandukwala <ammar@ammar.io>
2023-02-25 00:28:52 +0000

    fix comments

--- cli/deployment/yaml.go
+++ cli/deployment/yaml.go
@@ -14,21 +14,25 @@ import (
 	"github.com/coder/coder/codersdk"
 )
 
-type yamlSchema map[string]interface{}
+// func goNameToYAML(name string) string {
+// }
 
-func yamlDeploymentField(field reflect.Value) (*yaml.Node, error) {
-	valueField := field.FieldByName("Value")
+func scalarNode(v any) (*yaml.Node, error) {
 	var valueStr string
-	switch v := valueField.Interface().(type) {
+	switch v := v.(type) {
 	case time.Duration:
 		valueStr = v.String()
 	case bool:
 		valueStr = strconv.FormatBool(v)
 	case string:
 		valueStr = v
+	case int:
+		valueStr = strconv.Itoa(v)
+	case int64:
+		valueStr = strconv.Itoa(int(v))
 	default:
 		return nil, xerrors.Errorf(
-			"unsupported DeploymentField type: %s", valueField.Type().String(),
+			"unsupported scalar type: %T", v,
 		)
 	}
 	return &yaml.Node{
@@ -37,10 +41,38 @@ func yamlDeploymentField(field reflect.Value) (*yaml.Node, error) {
 	}, nil
 }
 
+func yamlDeploymentField(field reflect.Value) (*yaml.Node, error) {
+	valueField := field.FieldByName("Value")
+	valueKind := valueField.Kind()
+
+	switch valueKind {
+	case reflect.Slice:
+		var content []*yaml.Node
+		for i := 0; i < valueField.Len(); i++ {
+			vi := valueField.Index(i)
+			n, err := scalarNode(vi.Interface())
+			if err != nil {
+				return nil, xerrors.Errorf("converting scalar slice element: %w", err)
+			}
+			content = append(content, n)
+		}
+		return &yaml.Node{
+			Kind:    yaml.SequenceNode,
+			Content: content,
+		}, nil
+	case reflect.Bool, reflect.Int, reflect.Int64, reflect.String:
+		return scalarNode(valueField.Interface())
+	default:
+		return nil, xerrors.Errorf("unsupported kind: %s", valueKind.String())
+	}
+}
+
 // MarshalYAML converts the deployment config to its MarshalYAML representation.
 func MarshalYAML(config *codersdk.DeploymentConfig) ([]byte, error) {
 	var (
-		schema      = yamlSchema{}
+		document = yaml.Node{
+			Kind: yaml.MappingNode,
+		}
 		configValue = reflect.ValueOf(config).Elem()
 		merr        *multierror.Error
 	)
@@ -53,17 +85,28 @@ func MarshalYAML(config *codersdk.DeploymentConfig) ([]byte, error) {
 
 		switch {
 		case strings.HasPrefix(typeName, "codersdk.DeploymentConfigField["):
+			// Write field name.
+			document.Content = append(document.Content, &yaml.Node{
+				Kind:        yaml.ScalarNode,
+				Value:       fieldName,
+				HeadComment: configField.FieldByName("Usage").String(),
+			})
 			node, err := yamlDeploymentField(configField)
 			if err != nil {
 				merr = multierror.Append(merr, err)
 				continue
 			}
-			schema[fieldName] = node
+			// Write node contents.
+			document.Content = append(document.Content, node)
 		default:
 			merr = multierror.Append(merr, xerrors.Errorf("unsupported type: %s", typeName))
 		}
 	}
-	byt, err := yaml.Marshal(schema)
-	merr = multierror.Append(merr, err)
+	byt, err := yaml.Marshal(document)
+	if err != nil {
+		merr = multierror.Append(
+			merr, xerrors.Errorf("marshal failed: %w\n%+v", err, document),
+		)
+	}
 	return byt, merr.ErrorOrNil()
 }
--- cli/deployment/yaml_test.go
+++ cli/deployment/yaml_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
 
 	"github.com/coder/coder/cli/deployment"
 )
@@ -13,8 +14,40 @@ func TestMarshalYAML(t *testing.T) {
 
 	t.Run("Default", func(t *testing.T) {
 		t.Parallel()
-		byt, err := deployment.MarshalYAML(deployment.DefaultConfig())
+		config := deployment.DefaultConfig()
+		// For testing array marshaling.
+		config.ProxyTrustedHeaders.Value = []string{"X-Forwarded-For", "X-Forwarded-Proto"}
+		byt, err := deployment.MarshalYAML(config)
 		t.Logf("yaml:\n%s", string(byt))
 		require.NoError(t, err)
+	})
+
+	t.Run("TestYAMLDecode", func(t *testing.T) {
+		t.Parallel()
+		var a struct {
+			B string
+			C string
+		}
+		a.B = "dog"
+		a.C = "cat"
+
+		b, err := yaml.Marshal(a)
+		require.NoError(t, err)
+
+		t.Logf("yaml\n%s", string(b))
+
+		var n yaml.Node
+		err = yaml.Unmarshal(b, &n)
+		require.NoError(t, err)
+
+		_, err = yaml.Marshal(n.Content[0])
+		require.NoError(t, err)
+
+		t.Logf("len(n.Content) = %d", len(n.Content))
+		t.Logf("n = %+v", n)
+
+		t.Logf("n.Content[0] = %+v", n.Content[0])
+		t.Logf("n.Content[0].Content[0] = %+v", n.Content[0].Content[0])
+		t.Logf("n.Content[0].Content[1] = %+v", n.Content[0].Content[1])
 	})
 }
